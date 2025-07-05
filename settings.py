b03240eb38b18c5b8694c9c53598e134c90bd27f
alyzee-chan <bayihagwethalyzeeaurore@gmail.com>
2025-07-06 00:28:33 +0100

    pages modifiées du site entierement responsive ayant pour seul probleme le logo qui ne s'affiche pas mais quand meme cliquable et operationnel

--- settings.py
+++ settings.py
@@ -137,3 +137,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    # Autres adresses autorisées si nécessaire (ex: votre domaine en production)
+]
